5cc4f7302be477b9060bf7eefd7c537b0e3a9dc7
David Jezreel F. Reyes <djfreyes@headoffice.corp.bpi.com.ph>
2025-12-02 16:08:39 +0800

    Initial Commit

---
+++ Activity5.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package ph.com.bpi.hello;
+
+/**
+ * 
+ */
+public class SGMS {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+		for (int i = 1; i <= 50; i++) {
+            sum += i;
+        }
+        System.out.println("sum: " + sum);
+	}// End main method
+}//End of class
